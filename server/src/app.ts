35a7571d2538046e5f333d5f84ba6ec580a19305
ashuXsingh <ashusingh5102@gmail.com>
2026-06-19 04:28:17 +0530

    setup typescript backend and user model

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    success: true,
+    message: "CloudVault API Running",
+  });
+});
+
+export default app;
