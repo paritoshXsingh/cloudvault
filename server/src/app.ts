8334eecc23aef75a6c982da8ed732d3d0cc90552
ashuXsingh <ashusingh5102@gmail.com>
2026-06-20 01:01:47 +0530

    implement jwt authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/authRoutes";
+
 const app = express();
 
 app.use(cors());
@@ -12,5 +14,7 @@ app.get("/", (_req, res) => {
     message: "CloudVault API Running",
   });
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ server/src/types/auth.types.ts
@@ -0,0 +1,6 @@
+import { Request } from "express";
+import { IUserDocument } from "./user.types";
+
+export interface AuthRequest extends Request {
+  user?: IUserDocument;
+}
